aeae7edbf1c2207ff8dc81ccc660fa9f0ca6caa6
Pedro Henrique de Almeida Peixoto <almeidapeixotopedro@gmail.com>
2026-09-17 08:25:09 -0300

    feat: add Enrollment entity

---
+++ GymCore.Domain/Enums/EnrollmentStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymCore.Domain.Enums
+{
+    public enum EnrollmentStatus
+    {
+        Pending,
+        Active,
+        Expired,
+        Cancelled,
+    }
+}
